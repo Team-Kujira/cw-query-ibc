10ef3d87f7c57fb80e4fd4dbc839637dd43d8a51
antstalepresh <stalepresh121@outlook.com>
2023-10-17 09:53:49 -0400

    remove err string from responses

--- contracts/kujira-ibc/src/binding.rs
+++ contracts/kujira-ibc/src/binding.rs
@@ -42,12 +42,10 @@ pub enum IbcQuery {
 #[cw_serde]
 pub struct VerifyMembershipResponse {
     pub is_valid: bool,
-    pub err: String,
 }
 
-/// VerifyMembershipResponse is data format returned from IbcRequest::VerifyNonMembership query
+/// VerifyNonMembershipResponse is data format returned from IbcRequest::VerifyNonMembership query
 #[cw_serde]
 pub struct VerifyNonMembershipResponse {
     pub is_valid: bool,
-    pub err: String,
 }
